ec28fa9fff2cc21b22bd556d01f391351bc7f6ad
officialtpss <officialtpss@gmail.com>
2019-02-06 08:54:52 +0530

    initial

---
+++ src/app/models/error.model.ts
@@ -0,0 +1,8 @@
+export class ErrorMessage {
+  error: HandleMessage
+}
+export class HandleMessage{
+  code: number;
+  message: string;
+  flag: string;
+}
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+  _id: string;
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+  points: number;
+  lastName: string;
+  created: Date;
+}
